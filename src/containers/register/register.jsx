027aaf444068db140004e9e2119e4e6f432551e5
zhaoyj <1650432983@qq.com>
2021-03-05 15:02:22 +0800

    修改antd-mobile配置

--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,8 +1,32 @@
 import React, {Component} from 'react'
+import {Radio, List, InputItem, NavBar, WingBlank, WhiteSpace} from 'antd-mobile'
+import Logo from "../../components/logo/logo";
 
+const ListItem = List.Item
 export default class Register extends Component {
 
     render() {
-        return (<div>Register</div>)
+        return (
+            <div>
+                <NavBar>硅&nbsp;谷&nbsp;聊&nbsp;天</NavBar>
+                <Logo></Logo>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户名：</InputItem>
+                        <WhiteSpace></WhiteSpace>
+                        <InputItem>密&nbsp;&nbsp;&nbsp;码：</InputItem>
+                        <WhiteSpace></WhiteSpace>
+                        <InputItem>确认密码：</InputItem>
+                        <WhiteSpace></WhiteSpace>
+
+                        <ListItem>
+                            <span>用户类型：</span>
+                            <Radio>老板</Radio>
+                            <Radio>大神</Radio>
+                        </ListItem>
+                    </List>
+                </WingBlank>
+            </div>
+        )
     }
 }
